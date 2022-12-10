f95263cb30647b1616e14d4730b03e17a1a5874f
Abdul Rehman <abdul.rehman@ucertify.com>
2022-12-10 17:23:03 +0530

    The data is almost started saving.......x2

--- testingdata.php
+++ testingdata.php
@@ -7,18 +7,24 @@ $answer = $_POST['answer'];
 $newans = strip_tags($answer);
 $newques = $_SESSION['question'] = $question;
 $_SESSION['answer'] = $newans;
+
+
+
+//$_SESSION = array_merge($_SESSION, $ans);
+
 // print_r($_SESSION);
-
+// echo $newques;
 //echo $_SESSION['question'];
-$inp = file_get_contents('question.json');
-$tempArray = json_decode($inp);
-$newarr = json_decode($tempArray[0]->content_text);
-array_push($newarr->answers,($_SESSION['answer']));
-
-// file_put_contents("question.json", array_push($newarr->answers, ($_SESSION['answer'])));
+// $inp = file_get_contents('question.json');
+// $tempArray = json_decode($inp);
+// $newarr = json_decode($tempArray[$newques+1]->content_text);
+// array_push($newarr->answers,($_SESSION['answer']));
 
 
-// file_put_contents("hi.json", json_encode($_POST));
+ print_r($_SESSION);
+
+
+file_put_contents("hi.json",json_encode($_SESSION));
 
 // $newarr = array($_SESSION['answer'], $_SESSION['question']);
 
