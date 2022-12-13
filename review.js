d2d28c2544db5e85a173d10f2c9dd4a452430ab8
Abdul Rehman <abdul.rehman@ucertify.com>
2022-12-13 18:07:01 +0530

    DOne this one

--- review.js
+++ review.js
@@ -1,54 +1,71 @@
 $(document).ready(function () {
   //Jquery here...
   var k = 0;
-  $.ajax({
-    url: "http://localhost/PHP_PROJECT/question.json",
-    type: "POST",
-    success: function (data) {
-      var str = JSON.parse(data[0]["content_text"]);
-      $(".question").text(str.question);
-      $(".options").empty();
+    $.ajax({
+        url: "http://localhost/PHP_PROJECT/question.json",
+        type: "POST",
+        success: function (data) {
+            var str = JSON.parse(data[0]["content_text"]);
+            $(".question").text(str.question);
+            $(".snippet").append(str.explanation);
+           
 
-      for (i = 0; i < 4; i++) {
-        $(".options").append(
-          '<div class="form-check"><input type="radio" class="form-check-input" id="radio' +
-          (i + 1) +
-          '"name="optradio" value="' +
-          i +
-          '"><label class= "answer form-check-label d-block" for= "' +
-          JSON.parse(data[0]["content_text"]).answers[i].answer +
-          '">' +
-          str.answers[i].answer +
-          "</label></div>"
-        );
-      }
+            $(".options").empty();
 
-      $(".form-check-input").click(function () {
-        sessionStorage.setItem("option0", $(this).attr("value"));
-      });
-      itemValue = sessionStorage.getItem("option0");
-      //   console.log(itemValue);
-      if (itemValue !== null) {
-        $('input[value="' + itemValue + '"]').click();
-      }
+            for (i = 0; i < 4; i++) {
+                $(".options").append(
+                    '<div class="form-check"><input type="radio" class="form-check-input" id="radio' +
+                    (i + 1) +
+                    '"name="optradio" value="' +
+                    i +
+                    '"><label class= "answer form-check-label d-block" for= "' +
+                    JSON.parse(data[0]["content_text"]).answers[i].answer +
+                    '">' +
+                    str.answers[i].answer +
+                    "</label></div>"
+                );
+        
+            }
 
+            $(".form-check-input").click(function () {
+                sessionStorage.setItem("option0", $(this).attr("value"));
+            });
+            itemValue = sessionStorage.getItem("option0");
+            //   console.log(itemValue);
+            if (itemValue !== null) {
+                $('input[value="' + itemValue + '"]').click();
+
+            }
+            $(".form-check-input").attr("disabled", "true");
+             
+            if (sessionStorage.getItem("result" + k) == 1) {
+              $(".checkit").append(
+                '<p class="alert alert-success d-flex justify-content-center">correct</p>'
+              );
+            } else if (sessionStorage.getItem("result" + k) == 0) {
+              $(".checkit").append(
+                '<p class="alert alert-warning d-flex justify-content-center">Incorrect</p>'
+              );
+            } else {
+              $(".checkit").append(
+                '<p class="alert alert-danger d-flex justify-content-center">Unattempt</p>'
+              );
+            }
+
+         
+
+        
       $(".data").text("0" + 1 + " of " + data.length);
       if (k == 0) {
         $(".prev").prop("disabled", true);
       } // disabling prev button by default
 
-      //for result page
-  
-      
-      
-
-        
-      
 
       //Everything on pressing next
       $(".next").click(function () {
         $(".options").empty();
-
+          $(".checkit").empty();
+          $(".snippet").empty();
         $(".prev").prop("disabled", false);
         k = k + 1;
         if (k > data.length - 1) {
@@ -59,15 +76,15 @@ $(document).ready(function () {
           for (i = 0; i < 4; i++) {
             $(".options").append(
               '<div class="form-check"><input type="radio" class="form-check-input" id="radio' +
-              (i + 1) +
-              '" name="optradio" value="' +
-              i +
-              '"><label class="answer form-check-label d-block" for="' +
-              JSON.parse(data[k]["content_text"]).answers[i].answer +
-              '">' +
-              JSON.parse(data[k]["content_text"]).answers[i].answer +
-              "</label></div>"
-            );
+                (i + 1) +
+                '" name="optradio" value="' +
+                i +
+                '"><label class="answer form-check-label d-block" for="' +
+                JSON.parse(data[k]["content_text"]).answers[i].answer +
+                '">' +
+                JSON.parse(data[k]["content_text"]).answers[i].answer +
+                "</label></div>"
+              );
           }
           if (k <= 8) {
             $(".data").text("0" + (k + 1) + " of " + data.length);
@@ -99,14 +116,36 @@ $(document).ready(function () {
           });
           itemValue = sessionStorage.getItem("option" + k);
           if (itemValue !== null) {
-            $('input[value="' + itemValue + '"]').click();
-          }
-        }
-      });
+              $('input[value="' + itemValue + '"]').click();
+              
+            }
+            $(".form-check-input").attr("disabled", "true");
 
+              if (sessionStorage.getItem("result" + k) == 1) {
+                $(".checkit").append(
+                  '<p class="alert alert-success d-flex justify-content-center">correct</p>'
+                );
+              } else if (sessionStorage.getItem("result" + k) == 0) {
+                $(".checkit").append(
+                  '<p class="alert alert-warning d-flex justify-content-center">Incorrect</p>'
+                );
+              } else {
+                $(".checkit").append(
+                  '<p class="alert alert-danger d-flex justify-content-center">Unattempt</p>'
+                );
+            }
+            $(".snippet").append(JSON.parse(data[k]["content_text"]).explanation);
+            
+            
+          }
+          
+      });
+        
       //Everything on pressing prev
       $(".prev").click(function () {
-        $(".options").empty();
+          $(".options").empty();
+        $(".checkit").empty();
+          $('.snippet').empty();
         $(".next").prop("disabled", false);
         k = k - 1;
         if (k < 0) {
@@ -117,16 +156,18 @@ $(document).ready(function () {
           for (i = 0; i < 4; i++) {
             $(".options").append(
               '<div class="form-check"><input type="radio" class="form-check-input" id="radio' +
-              (i + 1) +
-              '" name="optradio" value="' +
-              i +
-              '"><label class="answer form-check-label d-block" for="' +
-              JSON.parse(data[k]["content_text"]).answers[i].answer +
-              '">' +
-              JSON.parse(data[k]["content_text"]).answers[i].answer +
-              "</label></div>"
-            );
-          }
+                (i + 1) +
+                '" name="optradio" value="' +
+                i +
+                '"><label class="answer form-check-label d-block" for="' +
+                JSON.parse(data[k]["content_text"]).answers[i].answer +
+                '">' +
+                JSON.parse(data[k]["content_text"]).answers[i].answer +
+                "</label></div>"
+              );  
+             
+            }
+            
           console.log(k);
           if (k <= 8) {
             $(".data").text("0" + (k + 1) + " of " + data.length);
@@ -157,38 +198,59 @@ $(document).ready(function () {
           itemValue = sessionStorage.getItem("option" + k);
           // console.log(itemValue);
           if (itemValue !== null) {
-            $('input[value="' + itemValue + '"]').click();
-          }
+              $('input[value="' + itemValue + '"]').click();              
+            }
+            $(".form-check-input").attr("disabled", "true");
+              if (sessionStorage.getItem("result" + k) == 1) {
+                $(".checkit").append(
+                  '<p class="alert alert-success d-flex justify-content-center">correct</p>'
+                );
+              } else if (sessionStorage.getItem("result" + k) == 0) {
+                $(".checkit").append(
+                  '<p class="alert alert-warning d-flex justify-content-center">Incorrect</p>'
+                );
+              } else {
+                  $(".checkit").append(
+                    '<p class="alert alert-danger d-flex justify-content-center">Unattempt</p>'
+                  );
+            }
+            $(".snippet").append(
+              JSON.parse(data[k]["content_text"]).explanation
+            );
+            
+        
+            
         }
       }); //End of prev click function
 
       //Prinitng the list (sidebar)
       for (n = 0; n < data.length; n++) {
-        optValue = sessionStorage.getItem("option"+n);
+        optValue = sessionStorage.getItem("option" + n);
         $(".list-group").append(
           '<p class="list-group-item snip list-group-item-action" style="cursor:pointer; margin-top:-10px; z-index: 1;" id="' +
-          n +
-          'li"><b>Ques :  ' +
-          (n + 1) +
-          " </b>" +
-          data[n]["snippet"] +
-          '<br><span class="badge rounded-pill text-bg-warning att' + n + '">unattempted</span></p>');
-        
-        // console.log(optValue);
-           if (optValue !== null) {
-             $(".att" + n).addClass("text-bg-success").text("attepmted").removeClass("text-bg-warning");
-           }
-    
-       
-      }
+            n +
+            'li"><b>Ques :  ' +
+            (n + 1) +
+            " </b>" +
+            data[n]["snippet"] +
+            '<br><span class="badge rounded-pill text-bg-warning att' +
+            n +
+            '">unattempted</span></p>'
+        );
 
-        
-    
-      
+        // console.log(optValue);
+        if (optValue !== null) {
+          $(".att" + n)
+            .addClass("text-bg-success")
+            .text("attepmted")
+            .removeClass("text-bg-warning");
+        }
+      }
 
       //This is iterating the list items
       $(".list-group-item").click(function () {
-        $(".options").empty();
+          $(".options").empty();
+          $('.snippet').empty();
         var qid = $(this).attr("id");
         var final_id = parseInt(qid);
         k = final_id;
@@ -240,10 +302,31 @@ $(document).ready(function () {
         $(".form-check-input").click(function () {
           sessionStorage.setItem("option" + k, $(this).attr("value"));
         });
+
         itemValue = sessionStorage.getItem("option" + k);
         if (itemValue !== null) {
-          $('input[value="' + itemValue + '"]').click();
-        }
+            $('input[value="' + itemValue + '"]').click();            
+          }
+          $(".form-check-input").attr("disabled", "true");
+
+           if (sessionStorage.getItem("result" + k) == 1) {
+             $(".checkit").append(
+               '<p class="alert alert-success d-flex justify-content-center">correct</p>'
+             );
+           } else if (sessionStorage.getItem("result" + k) == 0) {
+             $(".checkit").append(
+               '<p class="alert alert-warning d-flex justify-content-center">Incorrect</p>'
+             );
+           } else {
+             $(".checkit").append(
+               '<p class="alert alert-danger d-flex justify-content-center">Unattempt</p>'
+             );
+          }
+            $(".snippet").append(
+              JSON.parse(data[k]["content_text"]).explanation
+            );
+          
+          
       }); //End of iterating items function
 
       $(".form-check-input").click(function () {
@@ -267,6 +350,7 @@ $(document).ready(function () {
   $(".list-bt").click(function () {
     $(".list-group").toggle();
   });
+
   $(".itmlist").click(function () {
     $(".list-group").toggle();
   });
@@ -281,29 +365,3 @@ $(document).ready(function () {
   //Start of result page
   // $(".endtest").show(".result-page");
 });
-
-function startTimer(duration, display) {
-  var start = Date.now(),
-    diff,
-    minutes,
-    seconds;
-  function timer() {
-    diff = duration - (((Date.now() - start) / 1000) | 0);
-    minutes = (diff / 60) | 0;
-    seconds = diff % 60 | 0;
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-    display.textContent = minutes + ":" + seconds;
-    if (diff <= 0) {
-      start = Date.now() + 1000;
-    }
-  }
-  timer();
-  setInterval(timer, 1000);
-}
-
-window.onload = function () {
-  var tenMinutes = 60 * 10,
-    display = document.querySelector("#time");
-  startTimer(tenMinutes, display);
-};
